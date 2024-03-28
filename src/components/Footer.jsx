1eae2976af6868e7c944810dae53368989c1d280
Tejus Sahi <tejussahi07@gmail.com>
2024-03-28 09:39:39 +0530

    Updated Footer and github pages deployment
    Updated Footer and github pages deployment

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer>
-            <p>&copy; 2023 Project by Param Bura</p>
+            <p>&copy; 2023 Project by Tejus</p>
         </footer>
     );
 };
